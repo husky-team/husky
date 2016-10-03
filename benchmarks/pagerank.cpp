6fa19bf0000edf733e10944d8f2bb4e36a5e8ca1
Yuzhen Huang <huangyuzhen93@gmail.com>
2016-10-03 17:31:31 +0800

    [Bench] Add benchmarks folder (#43)
    Also Fix issue #46

---
+++ benchmarks/pagerank.cpp
@@ -0,0 +1,106 @@
+// Copyright 2016 Husky Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <string>
+#include <vector>
+
+#include "boost/tokenizer.hpp"
+
+#include "core/engine.hpp"
+#include "io/input/hdfs_line_inputformat.hpp"
+
+class Vertex {
+   public:
+    using KeyT = int;
+
+    Vertex() : pr(0.15) {}
+    explicit Vertex(const KeyT& id) : vertexId(id), pr(0.15) {}
+    const KeyT& id() const { return vertexId; }
+
+    // Serialization and deserialization
+    friend husky::BinStream& operator<<(husky::BinStream& stream, const Vertex& u) {
+        stream << u.vertexId << u.adj << u.pr;
+        return stream;
+    }
+    friend husky::BinStream& operator>>(husky::BinStream& stream, Vertex& u) {
+        stream >> u.vertexId >> u.adj >> u.pr;
+        return stream;
+    }
+
+    int vertexId;
+    std::vector<int> adj;
+    float pr;
+};
+
+void pagerank() {
+    husky::io::HDFSLineInputFormat infmt;
+    infmt.set_input(husky::Context::get_param("input"));
+
+    // Create and globalize vertex objects
+    auto& vertex_list = husky::ObjListFactory::create_objlist<Vertex>();
+    auto parse_wc = [&vertex_list](boost::string_ref& chunk) {
+        if (chunk.size() == 0)
+            return;
+        boost::char_separator<char> sep(" \t");
+        boost::tokenizer<boost::char_separator<char>> tok(chunk, sep);
+        boost::tokenizer<boost::char_separator<char>>::iterator it = tok.begin();
+        int id = stoi(*it++);
+        it++;
+        Vertex v(id);
+        while (it != tok.end()) {
+            v.adj.push_back(stoi(*it++));
+        }
+        vertex_list.add_object(std::move(v));
+    };
+    husky::load(infmt, parse_wc);
+    husky::globalize(vertex_list);
+
+    // Iterative PageRank computation
+    auto& prch =
+        husky::ChannelFactory::create_push_combined_channel<float, husky::SumCombiner<float>>(vertex_list, vertex_list);
+    int numIters = stoi(husky::Context::get_param("iters"));
+    using namespace std::chrono;
+    for (int iter = 0; iter < numIters; ++iter) {
+        auto t1 = steady_clock::now();
+        husky::list_execute(vertex_list, [&prch, iter](Vertex& u) {
+            if (iter > 0)
+                u.pr = 0.85 * prch.get(u) + 0.15;
+
+            if (u.adj.size() == 0)
+                return;
+            float sendPR = u.pr / u.adj.size();
+            for (auto& nb : u.adj) {
+                prch.push(sendPR, nb);
+            }
+        });
+        auto t2 = steady_clock::now();
+        auto time = duration_cast<duration<double>>(t2 - t1).count();
+        if (husky::Context::get_global_tid() == 0)
+            husky::base::log_msg("[Iter" + std::to_string(iter) + "] " + std::to_string(time) + "s elapsed.");
+    }
+}
+
+int main(int argc, char** argv) {
+    std::vector<std::string> args;
+    args.push_back("hdfs_namenode");
+    args.push_back("hdfs_namenode_port");
+    args.push_back("input");
+    args.push_back("iters");
+    if (husky::init_with_args(argc, argv, args)) {
+        husky::run_job(pagerank);
+        return 0;
+    }
+    return 1;
+}
