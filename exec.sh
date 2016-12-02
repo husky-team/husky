f3090b3bb392e7b6d4fdce3b39e9a71b8d276996
YANG Fan <iddmbr@gmail.com>
2016-12-02 15:27:13 +0800

    [Doc] Minor modification of exec.sh for better understanding (#162)

--- exec.sh
+++ exec.sh
@@ -6,4 +6,9 @@
 # worker3
 #
 MACHINE_CFG=
-time pssh -t 0 -P -h ${MACHINE_CFG} -x "-t -t" "ulimit -c unlimited && cd /tmp && ./$@"
+
+# This point to the directory where Husky binaries live.
+# If Husky is running in a cluster, this directory should be available
+# to all machines.
+BIN_DIR=
+time pssh -t 0 -P -h ${MACHINE_CFG} -x "-t -t" "cd $BIN_DIR && ./$@"
