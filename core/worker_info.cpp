36d2d15177166da76f435c6b98df53b0fd7c9d47
Yuzhen Huang <huangyuzhen93@gmail.com>
2016-12-12 14:40:54 +0800

    [Core][WorkerInfo] Support inconsecutive id (#173)

--- core/worker_info.cpp
+++ core/worker_info.cpp
@@ -21,31 +21,25 @@ namespace husky {
 
 void WorkerInfo::add_worker(int process_id, int global_worker_id, int local_worker_id, int num_hash_ranges) {
     // set global_to_proc_
-    if (global_to_proc_.size() <= global_worker_id)
-        global_to_proc_.resize(global_worker_id + 1, -1);
-    global_to_proc_[global_worker_id] = process_id;
+    ASSERT_MSG(global_to_proc_.find(global_worker_id) == global_to_proc_.end(), "global_worker_id already exists in global_to_proc_");
+    global_to_proc_.insert({global_worker_id, process_id});
 
     // set global_to_local_
-    if (global_to_local_.size() <= global_worker_id)
-        global_to_local_.resize(global_worker_id + 1);
-    global_to_local_[global_worker_id] = local_worker_id;
+    ASSERT_MSG(global_to_local_.find(global_worker_id) == global_to_local_.end(), "global_worker_id already exists in global_to_local_");
+    global_to_local_.insert({global_worker_id, local_worker_id});
 
     // set local_to_global_
-    if (local_to_global_.size() <= process_id)
-        local_to_global_.resize(process_id + 1);
-
-    if (local_to_global_[process_id].size() <= local_worker_id)
-        local_to_global_[process_id].resize(local_worker_id + 1);
-    local_to_global_[process_id][local_worker_id] = global_worker_id;
+    ASSERT_MSG(local_to_global_[process_id].find(local_worker_id) == local_to_global_[process_id].end(), "{process_id, local_worker_id} already exists in local_to_global_");
+    local_to_global_[process_id].insert({local_worker_id, global_worker_id});
 
     // set hash_ring_
     hash_ring_.insert(global_worker_id);
 
     // set processes and workers
-    if (std::find(processes.begin(), processes.end(), process_id) == processes.end())
-        processes.push_back(process_id);
-    if (std::find(workers.begin(), workers.end(), global_worker_id) == workers.end())
-        workers.push_back(global_worker_id);
+    if (processes_.find(process_id) == processes_.end())
+        processes_.insert(process_id);
+    if (workers_.find(global_worker_id) == workers_.end())
+        workers_.insert(global_worker_id);
 }
 
 void WorkerInfo::set_hostname(int process_id, const std::string& hostname) {
--- core/worker_info.hpp
+++ core/worker_info.hpp
@@ -19,6 +19,7 @@
 
 #include "base/exception.hpp"
 #include "core/hash_ring.hpp"
+#include "core/utils.hpp"
 
 namespace husky {
 
@@ -30,17 +31,33 @@ class WorkerInfo {
         return process_id_;
     }
 
-    inline int get_process_id(int global_worker_id) const { return global_to_proc_[global_worker_id]; }
+    inline int get_process_id(int global_worker_id) const { 
+        auto p = global_to_proc_.find(global_worker_id);
+        ASSERT_MSG(p != global_to_proc_.end(), "global_worker_id not found");
+        return p->second; 
+    }
 
-    inline int get_num_local_workers() const { return local_to_global_[process_id_].size(); }
+    inline int get_num_local_workers(int process_id) const { 
+        auto p = local_to_global_.find(process_id);
+        ASSERT_MSG(p != local_to_global_.end(), "process_id not found");
+        return p->second.size(); 
+    }
 
-    inline int get_num_local_workers(int process_id) const { return local_to_global_[process_id].size(); }
+    inline int get_num_local_workers() const { return get_num_local_workers(process_id_); }
 
-    inline const std::vector<int>& get_tids_by_pid(int pid) const { return local_to_global_[pid]; }
+    inline std::vector<int> get_tids_by_pid(int pid) const { 
+        auto p_local_to_global_map = local_to_global_.find(pid);
+        std::vector<int> tids;
+        tids.reserve(p_local_to_global_map->second.size());
+        for (auto& lid_tid : p_local_to_global_map->second) {
+            tids.push_back(lid_tid.second);
+        }
+        return tids;
+    }
 
-    inline const std::vector<int>& get_local_tids() const { return local_to_global_[process_id_]; }
+    inline std::vector<int> get_local_tids() const { return get_tids_by_pid(process_id_); }
 
-    inline const std::vector<int>& get_pids() const { return processes; }
+    inline std::vector<int> get_pids() const { return {processes_.begin(), processes_.end()}; }
 
     inline const std::string& get_hostname(int process_id) const {
         if (hostname_[process_id].size() == 0) {
@@ -48,28 +65,35 @@ class WorkerInfo {
         }
         return hostname_[process_id];
     }
+
     inline const std::vector<std::string> get_hostnames() const {
         return hostname_;
     }
 
-    inline int get_num_processes() const { return processes.size(); }
+    inline int get_num_processes() const { return processes_.size(); }
 
-    inline int get_num_workers() const { return workers.size(); }
+    inline int get_num_workers() const { return workers_.size(); }
 
-    inline const std::vector<int>& get_global_tids() const { return workers; }
+    inline std::vector<int> get_global_tids() const { return {workers_.begin(), workers_.end()}; }
 
-    HashRing* get_hash_ring() { return &hash_ring_; }
+    inline HashRing* get_hash_ring() { return &hash_ring_; }
 
     inline int local_to_global_id(int process_id, int local_worker_id) const {
-        return local_to_global_[process_id][local_worker_id];
+        auto p_local = local_to_global_.find(process_id);
+        ASSERT_MSG(p_local != local_to_global_.end(), "process_id not found");
+        auto p_global = p_local->second.find(local_worker_id);
+        ASSERT_MSG(p_global != p_local->second.end(), "process_id, local_worker_id not found");
+        return p_global->second;
     }
 
     inline int local_to_global_id(int local_worker_id) const {
-        return local_to_global_[process_id_][local_worker_id];
+        return local_to_global_id(process_id_, local_worker_id);
     }
 
     inline int global_to_local_id(int global_worker_id) const {
-        return global_to_local_[global_worker_id];
+        auto p = global_to_local_.find(global_worker_id);
+        ASSERT_MSG(p != global_to_local_.end(), "global_worker_id not found");
+        return p->second;
     }
 
     void add_worker(int process_id, int global_worker_id, int local_worker_id, int num_hash_ranges = 1);
@@ -79,12 +103,12 @@ class WorkerInfo {
     inline void set_process_id(int process_id) { process_id_ = process_id; }
 
    protected:
-    std::vector<int> global_to_proc_;
+    std::unordered_map<int,int> global_to_proc_;
     std::vector<std::string> hostname_;
-    std::vector<std::vector<int>> local_to_global_;
-    std::vector<int> global_to_local_;
-    std::vector<int> processes;
-    std::vector<int> workers;
+    std::unordered_map<int, std::unordered_map<int,int>> local_to_global_;
+    std::unordered_map<int,int> global_to_local_;
+    std::unordered_set<int> processes_;
+    std::unordered_set<int> workers_;
     HashRing hash_ring_;
     int process_id_ = -1;
 };
